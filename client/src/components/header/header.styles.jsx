d82c8b1d0b6734e84203ebe1d075510829d6228a
Sasha Galashka <galashka.mail@gmail.com>
2020-05-12 13:42:29 -0400

    added icon

--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -7,6 +7,12 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    padding: 10px;
+    margin-bottom: 20px;
+  };
 `;
 
 export const LogoContainer = styled(Link)`
@@ -15,6 +21,11 @@ export const LogoContainer = styled(Link)`
   padding: 25px;
   display: flex;
   align-items: center;
+
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    padding: 0;
+  };
 `;
 
 export const OptionsContainer = styled.div`
@@ -23,6 +34,10 @@ export const OptionsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  };
 `;
 
 export const OptionLink = styled(Link)`
